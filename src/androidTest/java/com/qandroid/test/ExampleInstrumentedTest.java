e896e97fcf3b6c466b8dd764f0956f215f13708f
chengxiang.peng <chengxiang.peng@qunar.com>
2017-01-02 02:17:12 +0800

    修改项目名称错误

--- src/androidTest/java/com/qandroid/test/ExampleInstrumentedTest.java
+++ src/androidTest/java/com/qandroid/test/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.qproject.test.test", appContext.getPackageName());
+        assertEquals("com.qandroid.test.test", appContext.getPackageName());
     }
 }
