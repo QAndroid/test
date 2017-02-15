aafdb9a9a77d8ef7bc615d7b0cf33c371c0728e5
chengxiang.peng <chengxiang.peng@qunar.com>
2017-02-15 18:11:21 +0800

    修复了leakcanary检测

--- src/main/java/com/qandroid/test/leakcanary/LeakCanaryFragment.java
+++ src/main/java/com/qandroid/test/leakcanary/LeakCanaryFragment.java
@@ -1,19 +1,19 @@
 package com.qandroid.test.leakcanary;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.qandroid.common.QBaseFragment;
 import com.qandroid.test.R;
 
 /**
  * Created by chengxiang.peng on 2017/1/3.
  */
 
-public class LeakCanaryFragment extends QBaseFragment {
+public class LeakCanaryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_leakcanary, container, false);
